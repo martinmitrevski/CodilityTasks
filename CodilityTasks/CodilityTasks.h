137a7109490230fdb5de3cf1b2f2a6e8434737fe
Martin Mitrevski <martin.mitrevski@netcetera.com>
2016-10-20 21:21:53 +0200

    Initial Commit

---
+++ CodilityTasks/CodilityTasks.h
@@ -0,0 +1,19 @@
+//
+//  CodilityTasks.h
+//  CodilityTasks
+//
+//  Created by Martin Mitrevski on 20/10/16.
+//  Copyright © 2016 Martin Mitrevski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodilityTasks.
+FOUNDATION_EXPORT double CodilityTasksVersionNumber;
+
+//! Project version string for CodilityTasks.
+FOUNDATION_EXPORT const unsigned char CodilityTasksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodilityTasks/PublicHeader.h>
+
+
